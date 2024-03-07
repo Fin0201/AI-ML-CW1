e3d453a711d0539768f24f4288875551d1274cbc
Finley Edwards <92788657+Fin0201@users.noreply.github.com>
2024-03-07 21:13:03 +0000

    Add diamond prediction

--- test_stuff.py
+++ test_stuff.py
@@ -1,4 +1,9 @@
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
 
 print("hi")
