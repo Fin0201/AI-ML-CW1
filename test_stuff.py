bfdc912053c265d2fff1d8e259b7043e59964f0b
Finley Edwards <92788657+Fin0201@users.noreply.github.com>
2024-03-12 15:32:25 +0000

    Changed model types

--- test_stuff.py
+++ test_stuff.py
@@ -4,17 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import (
-    accuracy_score,
-    classification_report,
-    confusion_matrix,
-    mean_squared_error,
-    r2_score,
-)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
 print("hi")
