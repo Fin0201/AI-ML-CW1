2df25767afd5708c33013e4970777b7855f300c8
Finley Edwards <92788657+Fin0201@users.noreply.github.com>
2024-03-12 15:00:13 +0000

    removed unused code

--- test_stuff.py
+++ test_stuff.py
@@ -3,10 +3,18 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    mean_squared_error,
+    r2_score,
+)
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.tree import DecisionTreeRegressor
 
 print("hi")
